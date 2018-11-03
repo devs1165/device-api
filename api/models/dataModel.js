be517130d263dfef8128db4e48ce0c171c6e2e0b
devil1165 <devkant@getambee.com>
2018-11-03 14:46:09 +0530

    data type changed to any in post api

--- api/models/dataModel.js
+++ api/models/dataModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dataSchema = mongoose.Schema({
     data:{ 
-        type:String, 
+        // type:String, 
         // required:true
     },
     timestamp:{
